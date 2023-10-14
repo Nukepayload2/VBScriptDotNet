a8fad5a19225a6c5e844a3b815b65aede5f996f0
Nukepayload2 <1939357182@qq.com>
2023-10-14 23:15:09 +0800

    Add Windows target

--- Interactive/vbi/Vbi.vb
+++ Interactive/vbi/Vbi.vb
@@ -12,6 +12,12 @@ Namespace Microsoft.CodeAnalysis.VisualBasic.Scripting.Hosting
         Friend Const InteractiveResponseFileName As String = "vbi.rsp"
 
         Public Shared Function Main(args As String()) As Integer
+#If WINDOWS7_0_OR_GREATER Then
+            System.Windows.Forms.Application.SetCompatibleTextRenderingDefault(False)
+            System.Windows.Forms.Application.SetHighDpiMode(System.Windows.Forms.HighDpiMode.PerMonitorV2)
+            System.Windows.Forms.Application.EnableVisualStyles()
+#End If
+
             Try
                 ' Note that AppContext.BaseDirectory isn't necessarily the directory containing vbi.exe.
                 ' For example, when executed via corerun it's the directory containing corerun.
